3de29dc142ad409685422a8c6325638f2ff16761
Koding <moritz.goeckel@gmail.com>
2015-06-26 14:21:15 +0000

    Added caching to the proxy

--- proxy.php
+++ proxy.php
@@ -1,5 +1,10 @@
 <?php
 
+//Caching
+require_once("phpfastcache/phpfastcache.php");
+phpFastCache::setup("storage","auto");
+$cache = phpFastCache();
+
 function url_get_contents ($Url) {
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $Url);
@@ -12,12 +17,19 @@ function url_get_contents ($Url) {
 if(isset($_GET['url'])){
     $url = urldecode($_GET['url']);
 
-    if(ini_get('allow_url_fopen'))
-        echo file_get_contents($url);
-    elseif(function_exists('curl_init'))
-    	echo url_get_contents ($url);
-    else
-	echo "allow_url_fopen and curl are not enabled on the server. Enable at least one of these for the proxy to work.";
+    $queryId = $_GET['url']);
+    $output = $cache->get($queryId);
+    if($output == null) {
+         if(ini_get('allow_url_fopen'))
+            $output = file_get_contents($url);
+        elseif(function_exists('curl_init'))
+    	    $output = url_get_contents ($url);
+        else
+	        $output = "allow_url_fopen and curl are not enabled on the server. Enable at least one of these for the proxy to work.";   
+        
+        $cache->set($queryId, $output, 60 * 60);
+    }
+    echo $output;
 }
 else {
     echo "Kein url parameter gefunden";
