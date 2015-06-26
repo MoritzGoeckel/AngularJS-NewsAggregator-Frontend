10bedf82f0109ac4c3c32978460ca1c13fcebe44
Koding <moritz.goeckel@gmail.com>
2015-06-26 14:15:55 +0000

    Addded a caching library and use it

--- api.php
+++ api.php
@@ -2,27 +2,56 @@
 
 header('Content-Type: application/json; charset=utf-8');
 
+//Caching
+require_once("phpfastcache/phpfastcache.php");
+phpFastCache::setup("storage","auto");
+$cache = phpFastCache();
+
 include("dbconnect.php");
 include("functions.php");
 
+//Example: api.php?mode=words&count=10&search=haus&date=20141120
 if($_GET['mode'] == "words" && isset($_GET['count']))
 {
-    echo(getWords($con, $_GET['count'], $_GET['date'], $_GET['search']));
-    //Example: api.php?mode=words&count=10&search=haus&date=20141120
+    $queryId = $_GET['count'] . "_" . $_GET['date'] . "_" . $_GET['search'];
+    $output = $cache->get($queryId);
+    if($output == null) {
+        $output = getWords($con, $_GET['count'], $_GET['date'], $_GET['search']);
+        $cache->set($queryId, $output, 60 * 60);
+    }
+    echo($output);
 }
+//Example: api.php?mode=articles&count=10&search=haus&date=20141120
 elseif($_GET['mode'] == "articles" && isset($_GET['count']))
 {
-    echo(getArticles($con, $_GET['count'], $_GET['search']));
-    //Example: api.php?mode=articles&count=10&search=haus&date=20141120
+    $queryId = $_GET['count'] . "_" . $_GET['search'];
+    $output = $cache->get($queryId);
+    if($output == null) {
+        $output = getArticles($con, $_GET['count'], $_GET['search']);
+        $cache->set($queryId, $output, 60 * 60);
+    }
+    echo($output);
 }
+//Example: http://minutus.de/news/api.php?mode=chart&word=is
 elseif($_GET['mode'] == "chart" && isset($_GET['word']))
 {
-    echo(getChart($con, $_GET['word']));
-    //Example: http://minutus.de/news/api.php?mode=chart&word=is
+    $queryId = $_GET['word'];
+    $output = $cache->get($queryId);
+    if($output == null) {
+        $output = getChart($con, $_GET['word']);
+        $cache->set($queryId, $output, 60 * 60 * 6);
+    }
+    echo($output);
 }
 elseif($_GET['mode'] == "timeframe" && isset($_GET['start']) && isset($_GET['end']) && isset($_GET['count']))
 {
-    echo(getWordsInTimeframe($con, $_GET['count'], $_GET['start'], $_GET['end']));
+    $queryId = $_GET['count'] . "_" . $_GET['start'] . "_" . $_GET['end'];
+    $output = $cache->get($queryId);
+    if($output == null) {
+        $output = getWordsInTimeframe($con, $_GET['count'], $_GET['start'], $_GET['end']);
+        $cache->set($queryId, $output, 60 * 60);
+    }
+    echo($output);
 }
 else
 {
